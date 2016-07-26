cd6a41fb636c1af637c0d787fad17d84275dc82a
Tony Mai <tonymaibox@yahoo.com>
2016-07-26 15:50:23 -0400

    updated from Tony's local files

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,4 +1,6 @@
 require 'rest-client'
+require 'json'
+require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
@@ -6,13 +8,27 @@ def get_character_movies_from_api(character)
   character_hash = JSON.parse(all_characters)
   # iterate ove the character hash to find the collection of `films` for the given
   #   `character`
+  collection_films = []  
+    character_hash['results'].each do |character_hash|
+      if character_hash['name'] == character
+        collection_films = character_hash['films']
+      end
+    end
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+  #======>Jasper-machine
+  film_data = []
+  film_restclient = []
+  collection_films.each do |film|
+    film_restclient = RestClient.get(film)
+    film_data << JSON.parse(film_restclient)
+  end
   # return value of this method should be collection of info about each film. 
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list 
   #  of movies by title. play around with puts out other info about a given film.
+film_data
 end
 
 def parse_character_movies(films_hash)
@@ -28,3 +44,4 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+get_character_movies_from_api("Luke Skywalker")
